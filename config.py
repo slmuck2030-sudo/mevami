9140f0d8038b2eccfa12776e608001bb3ea5c7b7
slmuck2030-sudo <slmuck2030@gmail.com>
2026-05-10 14:05:24 -0400

    Update setlist

--- config.py
+++ config.py
@@ -1,3 +1,3 @@
 #Check the readme to see how the path should look like on each OS
-SONGS_PATH ="/home/yourname/Clone Hero/Songs" 
-OS = "LINUX"
+SONGS_PATH = r"C:\Users\muckr\OneDrive\Documents\Clone Hero\Songs" 
+OS = "WINDOWS"
